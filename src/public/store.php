97ccdc6d4f7fd085e18c2196a9876c23e011aa37
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-08 16:07:18 +0900

    fix:ガード節を利用する

--- src/public/store.php
+++ src/public/store.php
@@ -12,27 +12,20 @@ $pdo = new PDO(
 $theme = $_POST['theme'] ?? '';
 $contents = $_POST['contents'] ?? '';
 
-$error = '';
-
 // タイトルと感想の入力チェック
 if ($theme === '' || $contents === '') {
-    $error = 'タイトルまたは学習のまとめが入力されていません。';
-}
-
-if (!$error) {
-    // データをDBに保存
-    $sql = 'INSERT INTO learningnotes (theme, contents, created_at) VALUES (:theme, :contents, NOW())';
-    $statement = $pdo->prepare($sql);
-    $statement->bindValue(':theme', $theme);
-    $statement->bindValue(':contents', $contents);
-    $statement->execute();
-
-    // 保存が完了したら、index.phpにリダイレクト
-    header('Location: index.php');
-    exit;
-} else {
-    echo $error . '<br>';
+    echo 'タイトルまたは学習のまとめが入力されていません。<br>';
     echo '<a href="index.php">トップページへ戻る</a>';
     exit;
 }
-?>
+
+// データをDBに保存
+$sql = 'INSERT INTO learningnotes (theme, contents, created_at) VALUES (:theme, :contents, NOW())';
+$statement = $pdo->prepare($sql);
+$statement->bindValue(':theme', $theme);
+$statement->bindValue(':contents', $contents);
+$statement->execute();
+
+// 保存が完了したら、index.phpにリダイレクト
+header('Location: index.php');
+exit;
